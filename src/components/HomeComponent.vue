026460c04464b95e2b542021544f1fcf6024e53b
7147_harshal <harshalnagtilak2809@gmail.com>
2024-04-28 08:00:16 +0530

    First Component is created

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Home Component</h1>
+</template>
+<script>
+
+
+    export default {
+        name:"HomeComponent",
+        components:{
+            
+        }
+
+    }
+</script>
+
+<style scoped>
+h1{
+    color: orange;
+}
+</style>
