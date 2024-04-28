904599d9f79642e22105c1d6050bafe5161affc6
7147_harshal <harshalnagtilak2809@gmail.com>
2024-04-28 09:26:09 +0530

    Interpolation and Data

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,16 +1,27 @@
 <template>
     <h1>Home Component</h1>
+    <h1>Email {{ email }}</h1>
+    <h1>Number {{ number }}</h1>
+    <h1>Name {{ getName("Harshal") }}</h1>
+    <h1>Address {{ address="Pune" }}</h1>
+
 </template>
 <script>
 
-
-    export default {
-        name:"HomeComponent",
-        components:{
-            
+export default {
+    name:"HomeComponent",
+    data(){
+        return{
+            email:"harshal@agkiya.com",
+            number:7756985642,
+            address:"Wakad",
+            getName(a){
+                return a
+            }
         }
-
     }
+}
+    
 </script>
 
 <style scoped>
